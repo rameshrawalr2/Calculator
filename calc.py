cf89b4c571f9f8ccfdf84a37a1e094b9c012a7da
rameshrawalr2 <34890987+rameshrawalr2@users.noreply.github.com>
2018-01-07 20:11:42 +0545

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 13:38:31 2018
+
+@author: ramesh
+"""
+
+def add(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a, b):
+    return a/b
---
+++ module.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 19:46:18 2018
+
+@author: ramesh
+"""
+
+from calc import add,sub
+print(add(4,5))
+print(sub(6,4))
+print(add(4,5)-sub(6,4))
+
+    
+
